eabfcb232abd8b675e3ed34f9a8dab522eec8431
richard jarram <rjarram@me.com>
2021-01-21 21:08:42 -0500

    compiler driven development: implemented threadpool

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,14 @@ use std::fs;
 
 fn main() {
   let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+  let pool = ThreadPool::new(5);
 
   for stream in listener.incoming() {
-      let stream = stream.unwrap();
+    let stream = stream.unwrap();
 
+    pool.execute(|| {
       handle_connection(stream);
+    });
   }
 }
 
@@ -23,12 +26,12 @@ fn handle_connection(mut stream: TcpStream) {
   let sleep = b"GET /sleep HTTP/1.1\r\n";
 
   let (status_line, filename) = if buffer.starts_with(get) {
-      ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
   } else if buffer.starts_with(sleep) {
-      thread::sleep(Duration::from_secs(5));
-      ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
+    thread::sleep(Duration::from_secs(5));
+    ("HTTP/1.1 200 OK\r\n\r\n", "hello.html")
   } else {
-      ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
+    ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "404.html")
   };
 
   let contents = fs::read_to_string(filename).unwrap();
